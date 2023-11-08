29c6ee502937e52e9d0a21c09e20fa60917c0f0e
Matthew Marelli <mattmarelli@gmail.com>
2023-11-08 08:58:41 -0800

    Started work on getting the website populated with information and looking good

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import './styles.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="fixed-to-top">
         <p>
           Welcome to Mary Ellen and Matt's wedding website!
         </p>
-        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          We have provided all our information below - if you have further questions, please send me a text at (425) 268-4119.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+        <p>
+          <span className='bold-word'> RSVP </span>
+        </p>
+        <a href='https://forms.gle/6XRd2AD5ZWynJErCA' target='_blank'>
+            Click here to RSVP!
+        </a>
+        <p>
+          Please RSVP by <span className='bold-word'> April 20th, 2024 </span>, so we can prepare accordingly for seating, food, and drinks.
+        </p>
+        <p>
+          <span className='bold-word'> Where </span>
+        </p>
+        <p>
+          The Barn on Wild Rose Prairie<br/>
+          21920 N Felspar Road<br/>
+          Deer Park, WA 99006<br/>
+          <a href='https://maps.app.goo.gl/uXDUMzZSZwzPRnfh9' target='_blank'>
+            The Barn on Wild Rose Prairie
+          </a>
+        </p>
       </header>
     </div>
   );
